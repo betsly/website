4d43745e53e0d53a6eb1228d43000e3020e7d6ee
LocoNico <71444545+LocoNico@users.noreply.github.com>
2020-11-19 14:10:37 +0100

    User implemented

--- src/main/java/at/kaindorf/beans/User.java
+++ src/main/java/at/kaindorf/beans/User.java
@@ -5,10 +5,64 @@
  */
 package at.kaindorf.beans;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author zmugg
  */
 public class User {
+    private String email;
+    private String username;
+    private String pw;
+    LocalDate accountCreationDate;
+
+    public User() {
+    }
+
+    public User(String email, String username, String pw, LocalDate accountCreationDate) {
+        this.email = email;
+        this.username = username;
+        this.pw = pw;
+        this.accountCreationDate = accountCreationDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public LocalDate getAccountCreationDate() {
+        return accountCreationDate;
+    }
+
+    public void setAccountCreationDate(LocalDate accountCreationDate) {
+        this.accountCreationDate = accountCreationDate;
+    }
+
+    @Override
+    public String toString() {
+        return username + " " + pw;
+    }
+    
     
 }
