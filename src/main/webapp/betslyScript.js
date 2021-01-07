65c32f749c4530db5742bfd7500ec85af265c3ef
SeOneAndOnly <71444626+SeOneAndOnly@users.noreply.github.com>
2021-01-07 16:10:51 +0100

    pswValidation

--- src/main/webapp/betslyScript.js
+++ src/main/webapp/betslyScript.js
@@ -1,7 +1,12 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function validate() {
+    let psw1 = document.getElementById("password").value;
+    let psw2 = document.getElementById("confirmation").value;
 
+    let success = false;
 
+    if (psw1 == psw2) {
+        
+    }
+
+    return success;
+};
